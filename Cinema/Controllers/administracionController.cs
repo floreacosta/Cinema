d46ea8023ec4eef29153df69531ff23325f410fe
floreacosta <florem.10@hotmail.com>
2017-05-24 22:40:33 -0300

    Inicializacion de tp - progra3

---
+++ Cinema/Controllers/administracionController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+  public class administracionController : Controller
+  {
+    //
+    // GET: /administracion/
+
+    public ActionResult inicio()
+    {
+      return View();
+    }
+
+		public ActionResult peliculas()
+		{
+			return View();
+		}
+
+		public ActionResult sedes()
+		{
+			return View();
+		}
+
+		public ActionResult carteleras()
+		{
+			return View();
+		}
+		public ActionResult reportes()
+		{
+			return View();
+		}
+	}
+}
---
+++ Cinema/Controllers/homeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+  public class homeController : Controller
+  {
+    //
+    // GET: /home/
+
+    public ActionResult inicio()
+    {
+      return View();
+    }
+
+		public ActionResult login()
+		{
+			return View();
+		}
+	}
+}
