123536d4d349256a38d74ecaa68b480f972118f6
floreacosta <florem.10@hotmail.com>
2017-06-05 22:09:37 -0300

    home

---
+++ Cinema/js/query.js
@@ -0,0 +1,13 @@
+﻿/*
+* Menu
+*/
+
+function abrirMenu() {
+	var menu = document.getElementById("menu");
+	menu.classList.add("menu-abierto");
+}
+
+function cerrarMenu() {
+	var menu = document.getElementById("menu");
+	menu.classList.remove("menu-abierto");
+}
